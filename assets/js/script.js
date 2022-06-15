5f6d09fd4cc31bc1f441111561bd0983ed7cd14e
Matheus Nascimento <nasc.matheusfrancisco@gmail.com>
2022-06-15 14:11:25 -0300

    corrigindo erro dos simbolos matematicos e fazendo o ponto funcionar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,34 @@
 const displayInput = document.getElementById('values');
 
+function dotClick(dotTag) {
+  const dot = dotTag.target.innerText;
+  const lastCharacter = displayInput.value[displayInput.value.length - 1];
+  if (lastCharacter === ' ' || displayInput.value === '') {
+    displayInput.value += `0${dot}`;
+  } else {
+    displayInput.value += dot;
+  }
+}
+
+function testSymbol() {
+  const symbol = displayInput.value[displayInput.value.length - 2];
+  if (symbol === '-' || symbol === '+' || symbol === '*' || symbol === '÷') {
+    return false;
+  }
+  return true;
+}
+
 function inputSymbol(symbolTag) {
   const symbol = symbolTag.target.innerText;
-  if (displayInput.value.length > 0) {
+  const test = testSymbol();
+  if (testSymbol() === true) {
+    if (symbol === '-') {
+      displayInput.value += ` ${symbol} `;
+    } else if (displayInput.value.length > 0) {
+      displayInput.value += ` ${symbol} `;
+    }
+  } else if (displayInput.value.length > 3) {
+    displayInput.value = displayInput.value.substring(0, displayInput.value.length - 3);
     displayInput.value += ` ${symbol} `;
   }
 }
@@ -30,6 +56,7 @@ function keyboardEventListeners() {
     const symbol = document.getElementById(symbols[index]);
     symbol.addEventListener('click', inputSymbol)
   }
+  document.getElementById('dot').addEventListener('click', dotClick);
 }
 
 window.onload = () => {
